c9c3ea93ef2f9aeb8ef6183bca32fdab9baf871b
Ismayil-13-Aliyev <aze.ismayil13@gmail.com>
2026-04-05 21:29:07 +0400

    class

---
+++ src/homework_13/Homework_13.java
@@ -0,0 +1,25 @@
+package homework_13;
+
+public class Homework_13 {
+
+	//1
+	
+	//?
+	
+	//2
+	
+	// zayti v papku i tam posmotret esli staraya skacat cerez google
+	
+	//3
+	
+	// ya ne smog nayti papku
+	
+	//4
+	
+	//?
+	
+	//5
+	
+	//?
+	
+}
